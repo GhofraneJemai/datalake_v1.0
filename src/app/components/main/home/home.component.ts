009aafe16f428b2dcab080e28ef26bd000f1492b
Ghofrane <jemaighofrane22@gmail.com>
2025-01-09 19:17:07 +0100

    first commit

--- src/app/components/main/home/home.component.ts
+++ src/app/components/main/home/home.component.ts
@@ -6,7 +6,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { CoreService } from '../../../core/core.service';
-import { LoginComponent } from '../../login/login.component';
+
 
 @Component({ selector: 'app-home', templateUrl: './home.component.html', styleUrl: './home.component.css' }) 
 export class HomeComponent implements OnInit{
